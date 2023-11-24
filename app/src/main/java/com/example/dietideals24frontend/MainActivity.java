3409a51d919df089881f4fc56dac68c58702667e
CarmineArena <carmin01@libero.ir>
2023-11-24 23:10:33 +0100

    do drawable for buttons and fields

--- app/src/main/java/com/example/dietideals24frontend/MainActivity.java
+++ app/src/main/java/com/example/dietideals24frontend/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment((new LogInFragment()));
         btnFragment = findViewById(R.id.btnSwitchLogInFragment);
         btnFragment.setOnClickListener(new View.OnClickListener() {
             @Override
