c635cd42b0e25866fd87aa5ef7f0b4b5f5748b33
CarmineArena <carmin01@libero.ir>
2023-11-24 19:23:50 +0100

    first commit

---
+++ app/src/main/java/com/example/dietideals24frontend/LogInFragment.java
@@ -0,0 +1,23 @@
+package com.example.dietideals24frontend;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LogInFragment extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_log_in, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/dietideals24frontend/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dietideals24frontend;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnFragment = findViewById(R.id.btnSwitchLogInFragment);
+        btnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnFragment.getText().equals("Sei un nuovo utente? Registrati")) {
+                    btnFragment.setText("Hai già un account? Accedi");
+                    replaceFragment(new SignUpFragment());
+                } else {
+                    btnFragment.setText("Sei un nuovo utente? Registrati");
+                    replaceFragment(new LogInFragment());
+                }
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+}
