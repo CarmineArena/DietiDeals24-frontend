80418c0ce62947a625251de25568ca147297714d
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2023-12-05 18:22:58 +0100

    Need to Resolve todos in SilentAuctionCreationFragment.java

---
+++ app/src/main/java/com/example/dietideals24frontend/utility/ImageUtils.java
@@ -0,0 +1,32 @@
+package com.example.dietideals24frontend.utility;
+
+import android.net.Uri;
+import android.content.Context;
+import android.content.ContentResolver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+    public static byte[] convertUriToByteArray(Context context, Uri uri) throws IOException {
+        ContentResolver contentResolver = context.getContentResolver();
+        InputStream inputStream = contentResolver.openInputStream(uri);
+
+        if (inputStream != null) {
+            ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            try {
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byteBuffer.write(buffer, 0, bytesRead);
+                }
+                return byteBuffer.toByteArray();
+            } finally {
+                inputStream.close();
+            }
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/example/dietideals24frontend/view/SilentAuctionCreationFragment.java
+++ app/src/main/java/com/example/dietideals24frontend/view/SilentAuctionCreationFragment.java
@@ -1,36 +1,44 @@
 package com.example.dietideals24frontend.view;
 
-import android.app.TimePickerDialog;
-import android.content.Intent;
-import android.os.Bundle;
+import static android.app.Activity.RESULT_OK;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.app.TimePickerDialog;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.MultiAutoCompleteTextView;
-import android.widget.TimePicker;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
 
+import android.widget.MultiAutoCompleteTextView;
+
+import com.bumptech.glide.Glide;
 import com.example.dietideals24frontend.R;
 import com.example.dietideals24frontend.modelDTO.User;
+import com.example.dietideals24frontend.utility.ImageUtils;
 
-import java.util.Calendar;
+import android.net.Uri;
+
 import java.util.Locale;
-
+import java.util.Calendar;
+import java.io.IOException;
 
 public class SilentAuctionCreationFragment extends Fragment {
-
-    View view;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private View view;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_silent_auction_creation, container, false);
 
+        // Retrieve LoggedIn User
         Bundle bundle = getArguments();
         User user = null;
         if (bundle != null) {
@@ -48,55 +56,67 @@ public class SilentAuctionCreationFragment extends Fragment {
         multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
         Button timeButton = view.findViewById(R.id.time_button);
-        final TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                // Formatta l'orario come desideri (ad esempio, in un formato HH:mm)
-                String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute);
-
-                // Puoi utilizzare selectedTime come necessario
-                timeButton.setText(selectedTime);
-            }
+        final TimePickerDialog.OnTimeSetListener timeSetListener = (view, hourOfDay, minute) -> {
+            // Format the time as you wish (for example, in an HH:mm format)
+            String selectedTime = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute);
+            timeButton.setText(selectedTime);
         };
-        timeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Calendar c = Calendar.getInstance();
-                int currentHour = c.get(Calendar.HOUR_OF_DAY);
-                int currentMinute = c.get(Calendar.MINUTE);
 
-                TimePickerDialog timePickerDialog = new TimePickerDialog(
-                        requireContext(),
-                        timeSetListener,
-                        currentHour,
-                        currentMinute,
-                        true  // Imposta true se desideri visualizzare il picker in modalità 24 ore
-                );
+        timeButton.setOnClickListener(v -> {
+            final Calendar c  = Calendar.getInstance();
+            int currentHour   = c.get(Calendar.HOUR_OF_DAY);
+            int currentMinute = c.get(Calendar.MINUTE);
 
-                timePickerDialog.show();
-            }
+            TimePickerDialog timePickerDialog = new TimePickerDialog(requireContext(), timeSetListener, currentHour, currentMinute,
+                    true  // Set to true if you want to visualize the picker 24 hours mode
+            );
+            timePickerDialog.show();
         });
 
-        Button yourButton = view.findViewById(R.id.next_button);
-        /*yourButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeScreen();
-            }
-        });*/
+        Button yourButton  = view.findViewById(R.id.next_button);
 
+        Button imageButton = view.findViewById(R.id.button);
+        imageButton.setOnClickListener(view -> openGallery());
 
         return view;
     }
 
-    private void changeScreen() {
-        // Crea un'Intent per avviare la tua nuova attività
-        Intent intent = new Intent(getActivity(), Home.class); //cambia home.class con la classe successiva
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
 
-        // Avvia la nuova attività
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+
+            // Converte l'URI in un byte array o un altro formato necessario
+            byte[] imageBytes = null;
+            try {
+                imageBytes = ImageUtils.convertUriToByteArray(getContext(), uri);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // TODO: 1. CREA L'ITEM E ASSOCIA L'ITEM ALL'UTENTE
+            // item.setImage(imageBytes);
+
+            // TODO: 2. Inserire nel DB il record relativo all'item creato (mandare tramite rest Api l'oggetto Item)
+            // uploadImageToServer(imageBytes);
+
+            // TODO: 3. Creare Asta, associargli l'oggetto e l'id del proprietario, e inserire nel DB l'istanza dell'asta (mandare tramite rest Api l'oggetto Auction)
+
+            ImageView imageView = view.findViewById(R.id.secondopera);
+            Glide.with(this) .load(imageBytes).into(imageView);
+        }
+    }
+
+    private void goBackToHome() {
+        Intent intent = new Intent(getActivity(), Home.class);
         startActivity(intent);
-
-        // Chiamando finish() puoi chiudere l'attuale attività se necessario
         requireActivity().finish();
     }
 }
