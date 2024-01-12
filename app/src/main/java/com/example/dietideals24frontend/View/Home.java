78a2007c92a70c6ef41ac972153b0e71408d1218
CarmineArena <carmin01@libero.ir>
2024-01-12 14:58:24 +0100

    Home recreated

--- app/src/main/java/com/example/dietideals24frontend/View/Home.java
+++ app/src/main/java/com/example/dietideals24frontend/View/Home.java
@@ -1,15 +1,21 @@
 package com.example.dietideals24frontend.View;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.TextView;
 import com.example.dietideals24frontend.R;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.dietideals24frontend.Model.User;
 import com.example.dietideals24frontend.Presenter.ActivityFactory;
+import com.example.dietideals24frontend.View.Dialog.HomeFragment;
 
-public class Home extends AppCompatActivity {
+/*public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +42,38 @@ public class Home extends AppCompatActivity {
             Intent intent1 = activityFactory.createIntentForSearchAuction(Home.this, user);
             startActivity(intent1);
         });
+    }
+}*/
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button btnCreate = findViewById(R.id.btnCreate);
+        replaceFragment(new HomeFragment());
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, CreateAuction.class);
+                startActivity(intent);
+            }
+        });
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, SearchAuctionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameGeneral,fragment);
+        fragmentTransaction.commit();
     }
 }
