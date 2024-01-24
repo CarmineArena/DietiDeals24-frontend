6e8af03f7abf16528df99eb6738f9647b9d8ff22
CarmineArena <carmin01@libero.ir>
2024-01-24 10:39:14 +0100

    now the auction activity shows the silent auction fragment

--- app/src/main/java/com/example/dietideals24frontend/View/Auction.java
+++ app/src/main/java/com/example/dietideals24frontend/View/Auction.java
@@ -1,10 +1,14 @@
 package com.example.dietideals24frontend.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.dietideals24frontend.R;
+import com.example.dietideals24frontend.SilentAuctionFragment;
 
 public class Auction extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class Auction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auction);
+        replaceFragment(new SilentAuctionFragment());
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentAuction,fragment);
+        fragmentTransaction.commit();
     }
 }
