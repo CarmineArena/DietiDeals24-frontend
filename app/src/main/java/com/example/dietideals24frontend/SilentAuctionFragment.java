15c48a74b43f39a06c523f33847501d5630c4e6b
CarmineArena <carmin01@libero.ir>
2024-01-24 10:35:32 +0100

    added fragment for silent auction

---
+++ app/src/main/java/com/example/dietideals24frontend/SilentAuctionFragment.java
@@ -0,0 +1,67 @@
+package com.example.dietideals24frontend;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SilentAuctionFragment extends Fragment {
+
+    View view;
+    String[] offer = {"Rialza di 10€", "Rialza di 20€", "Rialzo personalizzato"};
+    private List<String> offerList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_silent_auction, container, false);
+
+        TextView offerField = view.findViewById(R.id.offertField);
+        offerField.setVisibility(View.INVISIBLE);
+
+        Spinner spinnerType = view.findViewById(R.id.offerSpinner);
+
+        offerList = new ArrayList<>(Arrays.asList(offer));
+        // Crea un ArrayAdapter per le opzioni e collega l'array all'adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, offerList);
+
+        // Specifica il layout da utilizzare quando l'elenco delle opzioni viene visualizzato
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Collega l'adapter al tuo Spinner
+        spinnerType.setAdapter(adapter);
+
+        // Imposta un listener per gestire gli eventi di selezione
+        spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Rimuovi l'opzione "Scegli il tuo tipo di asta" se è stata selezionata un'altra opzione
+                String selectedType = offer[position];
+                if (selectedType.equals("Rialzo personalizzato")) {
+                    offerField.setVisibility(View.VISIBLE);
+                } else {
+                    offerField.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+
+        return view;
+    }
+}
