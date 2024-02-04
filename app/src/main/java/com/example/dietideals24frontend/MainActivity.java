d6333c30ef6786a71c717597c5946918fac12276
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2024-02-04 16:15:36 +0100

    .

--- app/src/main/java/com/example/dietideals24frontend/MainActivity.java
+++ app/src/main/java/com/example/dietideals24frontend/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnFragment;
     private TextView TextFragment;
 
+
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
